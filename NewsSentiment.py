8c48831d4ebfebd7cadffa8dd944206b3af7b01c
Scott Pickering <scottdpickering@gmail.com>
2018-02-23 15:30:49 +0000

    Article extracted from page before sentiment analysis

--- NewsSentiment.py
+++ NewsSentiment.py
@@ -6,9 +6,12 @@ client = textapi.Client("af398876", "a965f860dbd17f6c5958ed7ca395c737")
 class NewsSentiment:
 
     def getPolarity(self, url):
-
         
-        sentiment = client.Sentiment({'mode': 'document', 'url': url})
+        extract = client.Extract({'url': url})
+
+        text = extract['article']
+        
+        sentiment = client.Sentiment({'mode': 'document', 'text': text})
 
         if (sentiment['polarity'] == 'positive'):
             return 1
