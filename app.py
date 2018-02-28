0cebf0600749029e686d947bce680f4f870fe5c0
constantlyconfused <sam.hine27@gmail.com>
2018-02-28 18:24:50 +0000

    Correctly formatted tabbing

--- app.py
+++ app.py
@@ -7,33 +7,33 @@ try:
     urls = (
         '/', 'index'
         )
-        app = web.application(urls, globals())
-        render = web.template.render('templates/')
-
-        class index:
-            def GET(self):
-                return render.index()
-
-                def POST(self):
-                    data = json.loads(web.data())
-                    print(data)
-                    #Check for the type of request sent
-                    if data['type'] == "newMessage":
-                        #Handling for a message
-                        #a.addQuery(self, data['message'])
-                        result = main(data['message'])
-                        writeToFile()
-                        return result
-                        #return data['message']
-                    elif data['type'] == 'suggestMessages':
-                        #Handle suggested messages
-                        #return a.suggestCategories(self, 2)
-                        categories = suggestCategories()
-                        print(categories)
-                        return data['message']
-                    elif data['type'] == 'detectAnomalies':
-                        #return a.detectAnomalies(self)
-                        return data['message']
+    app = web.application(urls, globals())
+    render = web.template.render('templates/')
+    
+    class index:
+        def GET(self):
+            return render.index()
+        
+        def POST(self):
+            data = json.loads(web.data())
+            print(data)
+            #Check for the type of request sent
+            if data['type'] == "newMessage":
+                #Handling for a message
+                #a.addQuery(self, data['message'])
+                result = main(data['message'])
+                writeToFile()
+                return result
+                #return data['message']
+            elif data['type'] == 'suggestMessages':
+                #Handle suggested messages
+                #return a.suggestCategories(self, 2)
+                categories = suggestCategories()
+                print(categories)
+                return data['message']
+            elif data['type'] == 'detectAnomalies':
+                #return a.detectAnomalies(self)
+                return data['message']
 
     if __name__ == "__main__":
         app.run()
