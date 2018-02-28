6465fdc8d8316b54d8116f7a97a181d9ff129b72
constantlyconfused <sam.hine27@gmail.com>
2018-02-28 19:10:27 +0000

    Added string context to returned values

--- main.py
+++ main.py
@@ -85,19 +85,19 @@ def main(query):
     if intent=="Spot Price":
         if date=="" and time=="":
             print(DR.stock_price(company))
-            return DR.stock_price(company)
+            return "The current spot price of "+company+" is £"+str(DR.stock_price(company))
         elif date=="":
             print(DR.stock_price(company, time=time))
-            return DR.stock_price(company, time=time)
+            return "The spot price of "+company+" at "+time+" was £"+str(DR.stock_price(company, time=time))
         elif time=="":
             print(DR.stock_price(company, date))
-            return DR.stock_price(company, date)
+            return "The spot price of "+company+" on "+date+" was £"+str(DR.stock_price(company, date))
         else:
             print(DR.stock_price(company, date, time))
-            return DR.stock_price(company, date, time)
+            return "The spot price of "+company+" on "+date+" at "+time+" was £"+str(DR.stock_price(company, date, time))
     elif intent=="Market Capitalisation":
         print(DR.current_marketcap(company))
-        return DR.current_marketcap(company)
+        return "The current Market Capitalisation of "+company+" is £"+str(DR.current_marketcap(company))
     elif intent=="retrieve-news-company":
         returnarray=[]
         for article in DR.get_news(company):
@@ -119,40 +119,40 @@ def main(query):
     elif intent=="Open":
         if date!="" and date!=(datetime.now()).strftime("%Y-%m-%d"):
             print(DR.price_data(company, date)[0])
-            return DR.price_data(company, date)[0]
+            return "The open price of "+company+" on "+date+" was £"+str(DR.price_data(company, date)[0])
         else:
             print(DR.price_data_today(company)[0])
-            return DR.price_data_today(company)[0]
+            return "The opening price of "+company+" today was £"+str(DR.price_data_today(company)[0])
     elif intent=="Close":
         if date!="" and date!=(datetime.now()).strftime("%Y-%m-%d"):
             print(date)
             print(DR.price_data(company, date)[1])
-            return DR.price_data(company, date)[1]
+            return "The close price of "+company+" on "+date+" was £"+str(DR.price_data(company, date)[1])
         else:
             print(DR.price_data_today(company)[1])
-            return DR.price_data_today(company)[1]
+            return "The close price of "+company+" today was £"+str(DR.price_data_today(company)[1])
     elif intent=="High":
         if date!="" and date!=(datetime.now()).strftime("%Y-%m-%d"):
             print(DR.price_data(company, date)[2])
-            return DR.price_data(company, date)[2]
+            return "The high price of "+company+" on "+date+" was £"+str(DR.price_data(company, date)[2])
         else:
             print(DR.price_data_today(company)[2])
-            return DR.price_data_today(company)[2]
+            return "The high price of "+company+" today was £"+str(DR.price_data_today(company)[2])
         return
     elif intent=="Low":
         if date!="" and date!=(datetime.now()).strftime("%Y-%m-%d"):
             print(DR.price_data(company, date)[3])
-            return DR.price_data(company, date)[3]
+            return "The low price of "+company+" on "+date+" was £"+str(DR.price_data(company, date)[3])
         else:
             print(DR.price_data_today(company)[3])
-            return DR.price_data_today(company)[3]
+            return "The low price of "+company+" today was £"+str(DR.price_data_today(company)[3])
     elif intent=="Volume":
         if date!="" and date!=(datetime.now()).strftime("%Y-%m-%d"):
             print(DR.price_data(company, date)[4])
-            return DR.price_data(company, date)[4]
+            return "The volume traded of "+company+" on "+date+" was "+str(DR.price_data(company, date)[4])
         else:
             print(DR.price_data_today(company)[4])
-            return DR.price_data_today(company)[4]
+            return "The volume traded of "+company+" today is "+str(DR.price_data_today(company)[4])
     elif intent=="Percentage Change":
         listinput=list(jsonres['result']['parameters']['date-time'])
         if listinput==[]:
@@ -166,10 +166,10 @@ def main(query):
 
         if enddt=="":
             print(DR.diff(company, start=startdt))
-            return DR.diff(company, start=startdt)
+            return "Since "+startdt+", "+company+" has had a £"+str(DR.diff(company, start=startdt)[0])+" price change, and a "+str(DR.diff(company, start=startdt)[1])+"% change"
         else:
             print(DR.diff(company, start=startdt, end=enddt))
-            return DR.diff(company, start=startdt, end=enddt)
+            return "Between "+startdt+" and "+enddt+", "+company+" has had a £"+str(DR.diff(company, start=startdt, end=enddt)[0])+" price change, and a "+str(DR.diff(company, start=startdt, end=enddt)[1])+"% change"
     return ("This is the catch-all return at the end of the main function")
 
 #Function for determining start/end dates of difference function
